5e37aa9ae4dc570871f5610fc1d4a5c39ce5101f
alice94515 <37524906+alice94515@users.noreply.github.com>
2018-03-21 11:28:31 -0400

    Update statistics data fetch and calculation.py

--- statistics data fetch and calculation.py
+++ statistics data fetch and calculation.py
@@ -27,15 +27,63 @@ end=text.find("Last Split Date")
 usefultext=text[start+15:end+29]    # the string variable that contains all the statistics values
 print (usefultext)
 
-# next proceed to get the value of interested
 
+# functions to get the values of interest
+# Market Capital
+def market_cap(file):
+    location=file.find("Market Cap (intraday) 5")
+    i=0
+    length=len("Market Cap (intraday) 5")
+    while not file[location+length+i].isalpha():
+        i+=1
+    return file[location+length:location+length+i+1]
 
+# Trailing P/E
+def trailing_PE(file):
+    location=file.find("Trailing P/E")
+    length=len("Trailing P/E ")
+    i=0
+    while not file[location+length+i].isalpha():
+        i+=1
+    return file[location+length:location+length+i]
+#PEG ratio
+def PEG_ratio(file):
+    location=file.find("PEG Ratio (5 yr expected) ")
+    length=len("PEG Ratio (5 yr expected) ")
+    i=0
+    while not file[location+length+i].isalpha():
+        i+=1
+    return file[location+length:location+length+i]    
+# Debts Equity ratio
+def Debts_Equity_ratio(file):
+    location=file.find("Total Debt/Equity(mrq)")
+    length=len("Total Debt/Equity(mrq)")
+    i=0
+    while not file[location+length+i].isalpha():
+        i+=1
+    return file[location+length:location+length+max(i,3)]  
+# Enterprise Value / EBITDA
+def EBITDA(file):
+    location=file.find("EBITDA")
+    length=len("EBITDA ")
+    location=file.find("EBITDA",location+length)
+    i=0
+    while not file[location+length+i].isalpha():
+        i+=1
+    return file[location+length:location+length+max(i,3)] 
+# Beta
+def beta(file):
+    location=file.find("Beta")
+    length=len("Beta ")
+    return file[location+length:location+length+4] 
+# operating cash flow
+def cash_flow(file):
+    location=file.find("StatementOperating Cash Flow (ttm)")
+    i=0
+    length=len("StatementOperating Cash Flow (ttm)")
+    while not file[location+length+i].isalpha():
+        i+=1
+    return file[location+length:location+length+i+1]
 
     
 
-
-# clean html to useful text
-#for script in htmlfile(["script", "style"]):
-#for item in htmlfile:
-#    print(item)
-#print (htmlfile.stripped_string)
